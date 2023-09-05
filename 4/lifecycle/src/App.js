09124e87c182ba5841c4e2d87b36184e3aa37d12
Deniz000 <guldenizozdemir03@gmail.com>
2023-09-05 15:03:44 +0300

    4:1

--- 4/lifecycle/src/App.js
+++ 4/lifecycle/src/App.js
@@ -1,31 +1,13 @@
-import { useState, useEffect } from 'react'
-
-
+import Counter from "./components/Counter";
+import { useState } from "react";
 function App() {
-  const [number,setNumber] = useState(0);
-  const [name,setName] = useState('Mehmet');
+  const [isVisible, setIsVisible] = useState(true);
 
-  useEffect(()=>{
-    console.log('number state güncellendi');
-  },[number])
-
-
-  // useEffect(()=>{
-  //   console.log('number veya name state güncellendi');
-  // },[number, name])
-
-  useEffect(()=>{
-    console.log('Component mount edildi');
-  }, [])
   return (
-    <div className='App'>
-      <h1>{number}</h1>
-      <button onClick={()=>setNumber(number+1)}>arttır</button>
-      <hr/>
-
-      <h1>{name}</h1>
-      <button onClick={()=>setName("Mete")}>Click</button>
-
+    <div className="App">
+      {isVisible && 
+      <Counter/> }
+      <button onClick={()=>setIsVisible(!isVisible)} >Toglle</button>
     </div>
   );
 }
