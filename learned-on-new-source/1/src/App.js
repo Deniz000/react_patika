730e5a87e5762db5fa663fe34cd0df173ecdbf33
Deniz000 <guldenizozdemir03@gmail.com>
2023-09-11 16:48:12 +0300

    (extra source)

---
+++ learned-on-new-source/1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h3>{process.env.NODE_ENV}</h3> build edersen production yazar. npm start^ta development.  */}
+      
+    </div>
+  );
+}
+
+export default App;
