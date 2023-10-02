984c8127d8f27780eab1d52adc4359842fc22d18
Deniz000 <guldenizozdemir03@gmail.com>
2023-10-02 16:56:55 +0300

    hangman project

--- html_css_js_practice/project5/guess.js
+++ html_css_js_practice/project5/guess.js
@@ -1,8 +1,34 @@
-let buttons = document.getElementById('a');
-let units = 3; // units değişkenini tanımlayın ve gerekli değeri atayın
+// Sayfa yüklendiğinde çalışacak olan fonksiyon
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'
+    , 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'
+    , 'u', 'v', 'w', 'x', 'y', 'z'];
+window.onload = function () {
+    let buttons = document.getElementById("buttons");
+    let inputs = document.getElementById("inputs");
 
-for (let i = 0; i < units; i++) {
-    const newSquare = document.createElement("div");
-    newSquare.classList.add("wrapper");
-    buttons.appendChild(newSquare);
-}
+    for (let i = 0; i <= 19; i++) {
+        let div = document.createElement("div");
+        div.className = "box";
+        div.textContent = letters[i];
+        buttons.appendChild(div);
+    }
+
+    let separator = document.createElement("div");
+    separator.style.width = "100%"; 
+    separator.style.height = "10px"; // Ayırıcı için istediğiniz yüksekliği ayarlayabilirsiniz.
+    buttons.appendChild(separator);
+
+    for (let i = 20; i <= 25; i++) {
+        let div = document.createElement("div");
+        div.className = "box";
+        div.textContent = letters[i];
+        buttons.appendChild(div);
+    }
+
+    for(let i = 0; i <= 5; i++) {
+         let newInput = document.createElement('input');
+         newInput.type = "text";
+         newInput.className = 'input-letter'
+         inputs.appendChild(newInput);
+    }
+};
