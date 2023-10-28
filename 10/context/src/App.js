bfcfcb88c218d7cfbc7df353f20195fbaba4a435
Deniz000 <guldenizozdemir03@gmail.com>
2023-10-29 00:50:00 +0300

    get call

--- 10/context/src/App.js
+++ 10/context/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import Container from './components/Container';
 import { ThemeProvider } from './context/ThemeContext';
@@ -5,12 +6,21 @@ import { UserProvider } from './context/UserContext';
 
 function App() {
 
+  const [weather, setWeather] = useState(null)
+   React.useEffect(() => {
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=36.9863599&lon=35.3252861&appid=883489dfb6916d456613cafaa7dda332')
+    .then(res => res.json())
+    .then(data => setWeather(data))
+},[])
   return (
-    <ThemeProvider>
-      <UserProvider>
-        <Container />
-      </UserProvider>
-    </ThemeProvider>
+    <code>{JSON.stringify(weather.weather[0].main)}</code>
+
+    // <ThemeProvider>
+    //   <UserProvider>
+    //     <Container />
+    //   </UserProvider>
+    // </ThemeProvider>
   );
 }
 
